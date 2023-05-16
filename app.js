bbe089b33c489852e067ecc19a0e2726c43623d2
Noorulainshoaib <125483123+Noorulainshoaib@users.noreply.github.com>
2023-05-16 19:43:37 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+function getNum(num){
+    document.getElementById('result').value += num;
+}
+
+function clearNum(){
+    document.getElementById('result').value = '';
+}
+
+function removeNumb(){
+    var res = document.getElementById('result').value; 
+    document.getElementById('result').value = document.getElementById('result').value.slice(0,res.length-1)
+}
+
+function getResults(){
+    document.getElementById('result').value = eval(document.getElementById('result').value);
+} 
+function sqrt(){
+    document.getElementById("ro").innerHTML = Math.sqrt("val");
+    
+}
+function radians() {
+    display.value = display.value * (Math.PI / 180);
+  }
